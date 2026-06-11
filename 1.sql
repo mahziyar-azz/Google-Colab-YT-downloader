e9317e00dd66f6e327450c39626b2ecfee6ff970
mahziyar-azz <mahziyar.azimzadeh@gmail.com>
2026-06-11 16:29:44 +0330

    fix someting

--- 1.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    users.full_name AS 'نام مشتری',
-    COUNT(tickets.ticket_id) AS 'تعداد بلیت های خریداری شده'
-FROM users
-JOIN tickets ON users.user_id = tickets.user_id
-GROUP BY users.user_id
-ORDER BY COUNT(tickets.ticket_id) DESC;
